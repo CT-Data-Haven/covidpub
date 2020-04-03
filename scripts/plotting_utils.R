f385f244f6b4f3b80593c4dacc3f66c16684f2c1
camille-s <camille.seaberry@gmail.com>
2020-04-03 15:48:33 -0400

    Switch charts to billboardjs

--- scripts/plotting_utils.R
+++ scripts/plotting_utils.R
@@ -2,13 +2,15 @@ font_add("source", regular = "SourceSansPro-Regular.ttf", bold = "SourceSansPro-
 showtext_auto()
 
 theme_din2 <- function(...) {
-  camiller::theme_din(base_family = "source", base_size = 11, ...) + 
+  theme_gray(base_size = 11, ...) + 
     theme(legend.text = element_text(size = rel(0.75)), 
           legend.key.size = unit(0.8, "lines"), 
           legend.title = element_text(size = rel(0.9)),
+          panel.background = element_blank(),
+          panel.grid.major.y = element_line(color = "gray70"),
           plot.title = element_text(size = rel(1.1)),
           plot.subtitle = element_text(size = rel(0.9)))
 }
 
-update_geom_defaults("text", list(family = "source", fontface = "bold"))
+# update_geom_defaults("text", list(family = "source", fontface = "bold"))
 theme_set(theme_din2())
