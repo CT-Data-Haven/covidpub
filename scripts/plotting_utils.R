881c359481241a8f3509193aa3020fe8e9f66d82
camille-s <camille.seaberry@gmail.com>
2020-04-07 13:57:36 -0400

    Remove camiller dependency

--- scripts/plotting_utils.R
+++ scripts/plotting_utils.R
@@ -1,16 +1,24 @@
-font_add("source", regular = "SourceSansPro-Regular.ttf", bold = "SourceSansPro-Bold.ttf")
-showtext_auto()
-
-theme_din2 <- function(...) {
-  theme_gray(base_size = 11, ...) + 
-    theme(legend.text = element_text(size = rel(0.75)), 
-          legend.key.size = unit(0.8, "lines"), 
-          legend.title = element_text(size = rel(0.9)),
-          panel.background = element_blank(),
-          panel.grid.major.y = element_line(color = "gray70"),
-          plot.title = element_text(size = rel(1.1)),
-          plot.subtitle = element_text(size = rel(0.9)))
+clean_titles <- function(x) {
+  x <- stringr::str_replace_all(x, "_", " ")
+  x <- stringr::str_to_sentence(x)
+  x
 }
 
+
+# not currently using ggplot
+# font_add("source", regular = "SourceSansPro-Regular.ttf", bold = "SourceSansPro-Bold.ttf")
+# showtext_auto()
+# 
+# theme_din2 <- function(...) {
+#   theme_gray(base_size = 11, ...) + 
+#     theme(legend.text = element_text(size = rel(0.75)), 
+#           legend.key.size = unit(0.8, "lines"), 
+#           legend.title = element_text(size = rel(0.9)),
+#           panel.background = element_blank(),
+#           panel.grid.major.y = element_line(color = "gray70"),
+#           plot.title = element_text(size = rel(1.1)),
+#           plot.subtitle = element_text(size = rel(0.9)))
+# }
+
 # update_geom_defaults("text", list(family = "source", fontface = "bold"))
-theme_set(theme_din2())
+# theme_set(theme_din2())
