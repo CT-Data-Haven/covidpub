a144d941c4441076db9860dd27c98cb11baf47bf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-17 16:19:58 +0000

    Deploy CT-Data-Haven/covidpub to github.com/CT-Data-Haven/covidpub.git:gh-pages

--- covidsite.Rcheck/00_pkg_src/covidsite/scripts/acs.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(cwi)
-
-pops <- multi_geo_acs("B01003", regions = cwi::regions[c("Greater New Haven")]) %>%
-  janitor::clean_names()
-
-pops %>%
-  select(level, name, total_pop = estimate) %>%
-  write_csv("input_data/total_pop_2018.csv")
