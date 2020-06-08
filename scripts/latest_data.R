9c779028f48837931146c2b584e41546327af94e
camille-s <camille.seaberry@gmail.com>
2020-06-08 18:10:14 -0400

    Update latest_data.R to match new data sources

--- scripts/latest_data.R
+++ scripts/latest_data.R
@@ -1,6 +1,6 @@
 # get latest date of statewide, print to console to show in build tools
-county <- read.csv("input_data/covid_ct_county_detailed.csv")
-dates <- as.Date(county$date, format = "%m/%d/%y")
+county <- read.csv("input_data/covid_county.csv")
+dates <- as.Date(county$date)
 date_range <- format(range(dates), "%m/%d")
 cat("***********************************************\n")
 cat("Statewide data:", date_range[1], "to", date_range[2], "\n")
