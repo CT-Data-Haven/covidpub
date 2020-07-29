44a15de36c680eae8cfd2eed158968b7776c4e57
camille-s <camille.seaberry@gmail.com>
2020-07-29 15:01:19 -0400

    Finish case change table & chart; utility cleanup

--- scripts/plotting_utils.R
+++ scripts/plotting_utils.R
@@ -9,26 +9,27 @@ vivid <- c("#888888", rcartocolor::carto_pal(9, "Vivid")[sample.int(8)])
 seq_pal <- rcartocolor::carto_pal(5, "SunsetDark")
 # div_pal <- chroma::interp_colors(5, colors = c(pal[7], "#b8566d"))
 div_pal <- c("#52BCA3", "#7EA595", "#988E87", "#AA747A", "#B8566D")
+div_pal2 <- c('#25b697', '#8bc3a3', '#cdcfb0', '#d58491', '#cd4a6e')
 
 billboarder::set_theme("insight")
 
-# not currently using ggplot
-# font_add("source", regular = "SourceSansPro-Regular.ttf", bold = "SourceSansPro-Bold.ttf")
-# showtext_auto()
-# 
-# theme_din2 <- function(...) {
-#   theme_gray(base_size = 11, ...) + 
-#     theme(legend.text = element_text(size = rel(0.75)), 
-#           legend.key.size = unit(0.8, "lines"), 
-#           legend.title = element_text(size = rel(0.9)),
-#           panel.background = element_blank(),
-#           panel.grid.major.y = element_line(color = "gray70"),
-#           plot.title = element_text(size = rel(1.1)),
-#           plot.subtitle = element_text(size = rel(0.9)))
-# }
+d3_flag <- "function(value, ratio, id, index) {
+  return d3.format('+,')(value);
+}"
 
-# update_geom_defaults("text", list(family = "source", fontface = "bold"))
-# theme_set(theme_din2())
+d3_percent <- "function(value, ratio, id, index) {
+  return d3.format('+0.0%')(value);
+}"
+
+lt1 <- "
+  function(value) {
+    // return value < 0.01 ? '<1%' : d3.format('0.0%')(value);
+    var fmt = value < 0.1 ? '.1f' : '.0f';
+    return d3.format(fmt)(value * 100) + '%';
+  }
+"
+
+
 
 age_names <- function(x) {
   x <- stringr::str_replace_all(x, "(?<=\\d)_(?=\\d)", "-")
